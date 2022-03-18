b5b54068fa22533363d5204e71b576e26da47f63
Constantine Peresypkin <pconstantine@gmail.com>
2022-03-18 11:02:03 +0100

    add workflow_event input, drop `push` as default

--- dist/index.js
+++ dist/index.js
@@ -12,15 +12,19 @@ try {
   const octokit = github.getOctokit(core.getInput("github_token"));
   const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
   const branch = core.getInput("branch") || process.env.GITHUB_BASE_REF || process.env.GITHUB_REF_NAME || 'main'
+  const params = {
+    owner,
+    repo,
+    workflow_id: core.getInput("workflow_id"),
+    status: "success",
+    branch,
+  }
+  const event = core.getInput("workflow_event")
+  if (event) {
+    params.event = event
+  }
   octokit.actions
-    .listWorkflowRuns({
-      owner,
-      repo,
-      workflow_id: core.getInput("workflow_id"),
-      status: "success",
-      branch,
-      event: "push",
-    })
+    .listWorkflowRuns(params)
     .then((res) => {
       const lastSuccessCommitHash =
         res.data.workflow_runs.length > 0
